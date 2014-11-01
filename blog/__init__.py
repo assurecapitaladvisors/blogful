ea128af95baeb05021095d1f38f3659fa84b30ed
Geoff <assurecapitaladvisors@gmail.com>
2014-11-01 01:42:35 +0000

    Added some tests

--- blog/__init__.py
+++ blog/__init__.py
@@ -2,8 +2,6 @@ from flask import Flask
 import os
 
 app = Flask(__name__)
-
-app = Flask(__name__)
 config_path = os.environ.get("CONFIG_PATH", "blog.config.DevelopmentConfig")
 app.config.from_object(config_path)
 
